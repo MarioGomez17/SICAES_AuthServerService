53f2b3606879db875c9843590fd2e7ebdf07356d
mariog44 <mg441236@gmail.com>
2024-07-17 15:18:12 -0500

    AJUSTE DE LOS SERVICIOS. Se ajustaron los nombres de los parametros y los valores de retorno en los métodos del servicio de usuario.

--- src/main/java/AuthServerService/Services/ISessionService.java
+++ src/main/java/AuthServerService/Services/ISessionService.java
@@ -1,12 +1,10 @@
 package AuthServerService.Services;
 
-import java.util.List;
-
 import AuthServerService.Entities.UserEntity;
 
 public interface ISessionService {
 
-    public List<UserEntity> LogIn(String EmailUser, String PasswordUser) throws Exception;
+    public UserEntity LogIn(String IdentificationNumber, String Password_User) throws Exception;
 
-    public UserEntity LogOut() throws Exception;
+    public boolean LogOut() throws Exception;
 }
