0faa1615e6d84fb3c7b27ec96312e238f107ddf4
mariog44 <mg441236@gmail.com>
2024-07-17 19:25:07 -0500

    AJUSTE DE SEGURIDAD. Se ajusto la configuración de la seguridad señalando el UserDetails y definiendo la encriptación de la contraseña.

--- src/main/java/AuthServerService/Security/SecurityConfig.java
+++ src/main/java/AuthServerService/Security/SecurityConfig.java
@@ -12,18 +12,22 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 
+import AuthServerService.ServicesImplementation.SessionServiceImplementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
@@ -35,7 +39,6 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
@@ -43,6 +46,10 @@ import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	@Autowired
+	private SessionServiceImplementation Service;
+
 	@Bean
 	@Order(1)
 	SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
@@ -72,21 +79,22 @@ public class SecurityConfig {
 		return http.build();
 	}
 
-	/*@Bean
+	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}*/
+	}
 
 	@Bean
-	UserDetailsService userDetailsService() {
-		//PasswordEncoder Encoder = passwordEncoder();
-		UserDetails userDetails = User.builder()
-				.username("pepito")
-				//.password(Encoder.encode("Pepito12345"))
-				.password("{noop}12345")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(userDetails);
+	AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider AuthenticationProvider = new DaoAuthenticationProvider();
+		AuthenticationProvider.setUserDetailsService(Service.userDetails());
+		AuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		return AuthenticationProvider;
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 
 	@Bean
