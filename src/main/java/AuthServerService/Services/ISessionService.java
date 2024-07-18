c38db8275144befd0c60334e8929bffef0395960
mariog44 <mg441236@gmail.com>
2024-07-17 19:27:09 -0500

    AJUSTE DEL SERVICIO. Se eliminaron los métodos que no son necesarios y solo se dejo el que devuelve el UserDetails.

--- src/main/java/AuthServerService/Services/ISessionService.java
+++ src/main/java/AuthServerService/Services/ISessionService.java
@@ -1,10 +1,8 @@
 package AuthServerService.Services;
 
-import AuthServerService.Entities.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface ISessionService {
 
-    public UserEntity LogIn(Long IdentificationNumber_User, String Password_User) throws Exception;
-
-    public boolean LogOut() throws Exception;
+    public UserDetailsService userDetails();
 }
